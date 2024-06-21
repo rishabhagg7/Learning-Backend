b09f2fa81aade35898e3c81578503793285f0714
Rishabh Aggarwal <code.rishabh712@gmail.com>
2024-06-21 19:25:22 +0530

    connect to mongo atlas

---
+++ youtube-backend/src/index.js
@@ -0,0 +1,27 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path:"./env"
+})
+
+connectDB()
+/*
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error: ",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`APP is listening on PORT ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("ERROR: ",error);
+        throw error
+    }
+})()
+*/
