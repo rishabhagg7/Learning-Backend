28d785dd24aa829bb1a9f36dda483c1465a52731
Rishabh Aggarwal <code.rishabh712@gmail.com>
2024-06-24 02:35:06 +0530

    util code added

--- youtube-backend/src/index.js
+++ youtube-backend/src/index.js
@@ -6,7 +6,19 @@ dotenv.config({
     path:"./env"
 })
 
+//async method return a promise
 connectDB()
+.then(()=>{
+    //use app
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(` Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(` MongoDB connection failed!! ${err}`);
+})
+
+
 /*
 ;(async ()=>{
     try {
