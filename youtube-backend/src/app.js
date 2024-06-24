54514c75665b28f88ca97aaf0756c3999736fb0d
Rishabh Aggarwal <code.rishabh712@gmail.com>
2024-06-24 21:10:47 +0530

    all setup configured

--- youtube-backend/src/app.js
+++ youtube-backend/src/app.js
@@ -15,4 +15,12 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public")) //store file,folder,images on our server
 app.use(cookieParser())
 
+// routes
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:8000/api/v1/users/register 
+
 export {app}
--- youtube-backend/src/index.js
+++ youtube-backend/src/index.js
@@ -1,10 +1,13 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from './db/index.js'
+import {app} from "./app.js"
 
 dotenv.config({
     path:"./env"
 })
+
+
 
 //async method return a promise
 connectDB()
