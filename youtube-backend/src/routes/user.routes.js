55bfce84012256821d4592ab0267e3dea0546597
Rishabh Aggarwal <code.rishabh712@gmail.com>
2024-06-26 02:19:51 +0530

    Add refresh access token functionality

--- youtube-backend/src/routes/user.routes.js
+++ youtube-backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js"
+import { loginUser, logoutUser, refresAccessToken, registerUser } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJwt } from "../middlewares/auth.middleware.js"
 
@@ -25,5 +25,6 @@ router.route("/login").post(loginUser)
 
 // secured routes
 router.route("/logout").post(verifyJwt,logoutUser)
+router.route("/refresh-token").post(refresAccessToken)
 
-export default router
+export default router 
