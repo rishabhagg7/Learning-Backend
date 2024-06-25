44cfaf6df4dec1405d39dd0ab05c3f564c5021fa
Rishabh Aggarwal <code.rishabh712@gmail.com>
2024-06-25 16:35:30 +0530

    minor bug fix

--- youtube-backend/src/controllers/user.controller.js
+++ youtube-backend/src/controllers/user.controller.js
@@ -29,7 +29,7 @@ const registerUser = asyncHandler( async(req,res) => {
     ){
         throw new ApiError(400,"All fields are required")
     }
-    if(!email.match(validRegex)){
+    if(!email || !email.match(validRegex)){
         throw new ApiError(400,"Enter valid Email Id")
     }
     // res.status(200).json(
@@ -51,7 +51,11 @@ const registerUser = asyncHandler( async(req,res) => {
 
     // check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
     }
